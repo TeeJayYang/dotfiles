99dc30aabc19e04de96ed909666bfe652c95ba2a
TJ Yang <teejayyang35@gmail.com>
2018-01-04 11:44:01 -0500

    reorganzied files, add symlink script

---
+++ symlink.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+## Get current dirctory
+export DOTFILES_DIR
+DOTFILES_DIR=$(pwd)
+
+shopt -s nullglob
+shopt -s dotglob
+
+## Link to ~
+for folder in vim bash; do
+    for f in $DOTFILES_DIR/$folder/*; do
+        ln -sfv "$f" ~
+    done
+done
+
+## Link to .config/$folder
+for folder in i3 i3status; do
+    mkdir -pv ~/.config/$folder
+    for f in $DOTFILES_DIR/$folder/*; do
+        ln -sfv "$f" ~/.config/$folder
+    done
+done
+
